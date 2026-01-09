0ebb8b5092ad1f37f9ec75b3b1767a37674e0216
etnevelpriv <leventerastovits@gmail.com>
2026-01-09 22:37:08 +0100

    Betolt a jatek, vannak kulonbozo nehezsegek, mas hatterek toltodnek be.

--- flappyBird/MainWindow.xaml.cs
+++ flappyBird/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Numerics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,24 +18,59 @@ namespace flappyBird
     public partial class MainWindow : Window
     {
         int gravitacio = 1;
+        string aktivNehezseg = "";
 
         Random random = new Random();
 
         public MainWindow()
         {
             InitializeComponent();
-            double canvasMagassag = gameCanvas.ActualHeight;
-            double canvasSzelesseg = gameCanvas.ActualWidth;
-
         }
         private void newGameButton_Click(object sender, RoutedEventArgs e)
         {
             menuGrid.Visibility = Visibility.Collapsed;
-            gameCanvas.Visibility = Visibility.Visible;
+            palyaValasztoGrid.Visibility = Visibility.Visible;
         }
         private void achievementShowButton_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void palyaButtonNapos_Click(object sender, RoutedEventArgs e)
+        {
+            aktivNehezseg = "napos";
+            backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/napos.png"));
+            palyaValasztoGrid.Visibility = Visibility.Collapsed;
+            gameCanvas.Visibility = Visibility.Visible;
+            renderGameCanvas();
+        }
+
+        private void palyaButtonEsos_Click(object sender, RoutedEventArgs e)
+        {
+            aktivNehezseg = "esos";
+            backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/esos.png"));
+            palyaValasztoGrid.Visibility = Visibility.Collapsed;
+            gameCanvas.Visibility = Visibility.Visible;
+            renderGameCanvas();
+        }
+
+        private void palyaButtonKodos_Click(object sender, RoutedEventArgs e)
+        {
+            aktivNehezseg = "kodos";
+            backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Images/kodos.png"));
+            palyaValasztoGrid.Visibility = Visibility.Collapsed;
+            gameCanvas.Visibility = Visibility.Visible;
+            renderGameCanvas();
+        }
+
+        private async void renderGameCanvas()
+        {
+            await Task.Delay(10);
+            double canvasMagassag = gameCanvas.ActualHeight;
+            double canvasSzelesseg = gameCanvas.ActualWidth;
+            egerText.Text = canvasMagassag.ToString();
+            double egerTop = canvasMagassag / 2;
+            Canvas.SetTop(eger, egerTop);
         }
     }
 }
